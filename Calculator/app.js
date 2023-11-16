78159d1b01f2f12ba1d325b9f0e41d9cbbb735e5
Rana-Tek <ranatekbas122@gmail.com>
2023-11-16 15:59:08 +0300

    app js multiply

--- Calculator/app.js
+++ Calculator/app.js
@@ -29,5 +29,24 @@ for (let i = 0; i < number.length; i++ ) {
 result.addEventListener("click", function(){
     let inputString = input.innerHTML;
     let numbers = inputString.split(/\+|\-|\×|\÷/g);
-    
+    let operators = inputString.replace(/[0-9]|\./g, "").split("");
+
+    console.log(inputString);
+    console.log(operators);
+    console.log(numbers);
+    console.log("-----------------");
+
+    let divide = operators.indexOf("÷");
+    while(divide != 1) {
+        numbers.splice(divide, 2, numbers[divide] / numbers[divide+1]);
+        operators.splice(divide, 1);
+        divide = operators.indexOf("÷");
+    }
+
+    let multiply = operators.indexOf("x");
+    while (multiply != -1) {
+        numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
+        operators       
+    }
+
 })
